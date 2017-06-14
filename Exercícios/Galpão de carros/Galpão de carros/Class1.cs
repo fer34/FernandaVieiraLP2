1cfd0edae98a8b685ea495c7c884e6c9baf8b1c0
Fernanda Vieira <satellite274@gmail.com>
2017-06-14 00:01:30 -0300

    Terminados

---
+++ Exercícios/Galpão de carros/Galpão de carros/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galpão_de_carros
+{
+    class Carro
+    {
+        public string mol;
+        public double km;
+        public double pot, novo;
+        public int anocar;
+
+        public string Classificar()
+        {
+            string ida, pote;
+
+            if (km <= 5000)
+            {
+                ida = "Novo";
+            }
+
+            else if (km >= 3000)
+            {
+                ida = "Seminovo";
+            }
+
+            else
+            {
+                ida = "Velho";
+            }
+        
+            
+
+            if (pot >= 200)
+            {
+                pote = "Potente";
+            }
+
+            else if (pot < 200 && pot >= 120)
+            {
+                pote = "Forte";
+            }
+
+            else
+            {
+                pote = "Popular";
+            }
+
+            return String.Format("{0} - {1} - {2}", mol, ida, pote);
+        }
+
+        public double taxa()
+        {
+            return km / (DateTime.Now.Year - anocar);
+        }
+
+        public void attkm(double novo)
+        {
+            km = novo;
+        }
+    }
+}
---
+++ Exercícios/Galpão de carros/Galpão de carros/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galpão_de_carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q;
+
+            Console.WriteLine("Informe o número de carros no galpão");
+            q = int.Parse(Console.ReadLine());
+
+            Carro[] carrocho = new Carro[q];
+
+            for (int i = 0; i < q; i++)
+            {
+                carrocho[i] = new Carro();
+
+                Console.WriteLine("\nEscreva o modelo do carro");
+                carrocho[i].mol = Console.ReadLine();
+
+                Console.WriteLine("\nEscreva o número de km rodados");
+                carrocho[i].km = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nEscreva a potência do motor");
+                carrocho[i].pot = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nEscreva o ano de fabricação");
+                carrocho[i].anocar = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < q; i++)
+            {
+                Console.WriteLine(carrocho[i].Classificar());
+
+            }
+        }
+    }
+}
