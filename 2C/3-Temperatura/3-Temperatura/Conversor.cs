308e887d3d1b547e4f09bbdba41abd4435771c11
Fer <silvaanajuliasilva@hotmail.cok>
2017-08-02 15:56:18 -0300

    temperatura

---
+++ 2C/3-Temperatura/3-Temperatura/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Temperatura
+{
+    class Conversor
+    {
+        public double kelvin, celsius, fareirait;
+
+        public kelvin ()
+
+
+    }
+}
---
+++ 2C/3-Temperatura/3-Temperatura/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tempc;
+            string  tempdif;
+
+            
+
+            Console.WriteLine("Informe a tempeatura");
+            tempc = double.Parse(Console.ReadLine());
+
+            Conversor Conversão = new Conversor();
+
+            Console.WriteLine("Digite para qual você quer converter");
+            tempdif = Console.ReadLine();
+
+            if (tempdif == "kelvin")
+            {
+               
+            }
+            else
+            {
+
+            }
+            
+
+
+
+        }
+    }
+}
