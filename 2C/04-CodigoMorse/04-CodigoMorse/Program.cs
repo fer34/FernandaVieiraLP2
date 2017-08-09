0cf29f6b73cef1bb7669898a91eb74a9164e3670
trhht <satellite274@gmail.com>
2017-08-09 15:59:57 -0300

    Codigo morse para resolver

---
+++ 2C/04-CodigoMorse/04-CodigoMorse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CodigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string palavra;
+
+            Console.WriteLine("Escreva alguma coisa");
+            palavra = Console.ReadLine();
+
+            Console.WriteLine("O que deseja fazer com seu texto? \n1-Codificar \n2-Decodificar \n3-Transmitir \n4-Sair");
+            n = int.Parse(Console.ReadLine());
+
+            if (n == 1)
+            {
+               
+            }
+
+
+
+            
+
+
+        }
+    }
+}
