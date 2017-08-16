8af1090d1ccde9b2c2a918d7175cdec1ebaffffc
Fernanda <satellite274@gmail.com>
2017-08-16 16:03:47 -0300

    encapsulamentos

---
+++ 2C/5-Produtos/5-Produtos/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Produtos
+{
+    class Produto
+    {
+        private int id;
+        private int id { get { return id; } }
+
+        private int quantidade;
+
+        private int Quantidade { get { return quantidade; } }
+
+        public double Preco { get; set; }
+
+        private string nome;
+        public string nome { get { return nome; } }
+
+        public int Repor(int qtd)
+        {
+            this.quantidade += qtd;
+
+            return this.quantidade;
+        }
+
+        public void Retirar(int qtd)
+        {
+            if(this.quantidade >= qtd)
+            {
+                this.quantidade -= qtd;
+            }
+        }
+
+        else
+    {
+        thorw new expection
+    }
+        
+
+    
+}
---
+++ 2C/5-Produtos/5-Produtos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(1, "cajuzinho 20g", 0.50);
+            p.Reposiçao(40);
+            p.Retirada(10);
+
+            Console.WriteLine(p.Imprimir());
+            //p.retirada 90      //I WANNA make love In THE CLUB TANANANA IN THE CLUB TURURU 'V' HEY TUTUTU HEY, HEY, HEY 'V'/
+        }
+    }
+}
